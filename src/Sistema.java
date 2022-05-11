4c9f9db1eb1b202d6da435a264df576b7d3bb94f
daniel-fonseca <daniel@ccc.ufcg.edu.br>
2022-05-11 11:46:02 -0300

    aula 2 lcc3

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,5 +1,6 @@
 import exemplo.Lote;
 import exemplo.Produto;
+import exemplo.ProdutoRepository;
 
 public class Sistema {
 
@@ -7,8 +8,18 @@ public class Sistema {
 		
 		Produto produto = new Produto("Leite", "Parmalat");
 		Lote lote = new Lote(produto, 10L);
+		ProdutoRepository catalogo = new ProdutoRepository();
 		
 		System.out.println(produto);
 		System.out.println(lote);
+		
+		Produto leite = new Produto("Leite de saco", "Almir Zinho");
+		Produto pao = new Produto("Pão", "Nona");
+		Produto ovo = new Produto("Ovo", "Maely");
+		
+		catalogo.addProduto(pao);
+		catalogo.addProduto(ovo);
+		catalogo.addProduto(leite);
+		
 	}
 }
